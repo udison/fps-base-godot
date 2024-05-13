fd34712746c28115ec1e4914077184eb71c73645
Roque Santos <roque-100@hotmail.com>
2024-05-12 23:47:05 -0300

    added tweening to door open/close

--- scripts/gameplay/Door.gd
+++ scripts/gameplay/Door.gd
@@ -3,7 +3,12 @@ extends Node3D
 
 @onready var door = $Door
 
+@export var open_degrees: float = 120
+@export var time_to_open: float = 1 # in seconds
+
 var is_open := false
+var target_rotation: float = 0
+var tween: Tween
 
 
 func toggle_door_state(source = null):
@@ -11,13 +16,21 @@ func toggle_door_state(source = null):
 		close()
 	else:
 		open()
+		
+	if (tween):
+		tween.kill()
+	
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(door, 'rotation_degrees:y', target_rotation, 1)
 
 
 func open():
-	door.rotation.y = deg_to_rad(90)
+	target_rotation = open_degrees
 	is_open = true
 
 
 func close():
-	door.rotation.y = 0
+	target_rotation = 0
 	is_open = false
